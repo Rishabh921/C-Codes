44ad9b71001715fff5487afd9204571bde370b93
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2020-10-10 10:01:11 +0530

    Update BIn_to_Decimal.cpp

--- BIn_to_Decimal.cpp
+++ BIn_to_Decimal.cpp
@@ -3,13 +3,13 @@
 //it is a standard library function in C++ STL, 
 //it is used to convert a given string in vParameters:
 
-const string& str is an input string.
+//const string& str is an input string.
 
-size_t* idx is an optional parameter (pointer to the object whose value is set by the function), it's default value is 0 or we can assign it to nullptr.
+//size_t* idx is an optional parameter (pointer to the object whose value is set by the function), it's default value is 0 or we can assign it to nullptr.
 
-int base is also an optional parameter, its default is 10.
+//int base is also an optional parameter, its default is 10.
 //  It specifies the radix to determine the value type of input string (2 for binary, 8 for octal, 10 for decimal and 16 for hexadecimal).arious formats 
-    (like binary, octal, hex or a simple number in string formatted) into an integer.
+  //  (like binary, octal, hex or a simple number in string formatted) into an integer.
     
     
 //Syntax : int stoi (const string&  str, [size_t* idx], [int base]);
