80df2040e01e9ecca915d9908b3499ddab7261dd
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2020-10-20 11:15:04 +0530

    Update Vectors.cpp

--- Vectors.cpp
+++ Vectors.cpp
@@ -23,7 +23,14 @@ void print_vector_reference(vector<int> &myVector)
 }
 
 int main(){
-	vector<int> items; //to create vector
+	//to create vector 
+	vector<int> items; //without default size and default value
+	vector<int> v1(10); //with default size but not default value
+	vector<int> v2(10,0); //with default size(10) and default value(0)
+	vector<int> v3 {1,2,3}; //similar as int c[3] = {1,2,3}
+	vector<int> v4(v3.begin(),v3.end()); //initializing vector from another vector .
+	
+	
 	items.push_back(12); //to push elements in a vector
 	items.push_back(56); 
 	items.push_back(33);
